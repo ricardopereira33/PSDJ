0a8d18a58d559d8542116172670e6d1f7328e669
Tres <marcelo-lima96@hotmail.com>
2017-12-06 16:58:09 +0000

    mode funcs changed

--- Calculadora/src/main/java/calendar/Modes/ConverterMode.java
+++ Calculadora/src/main/java/calendar/Modes/ConverterMode.java
@@ -13,10 +13,6 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 
-/**
- *
- * @author Ricardo
- */
 public class ConverterMode implements Converter {
 
     @Override
@@ -25,16 +21,6 @@ public class ConverterMode implements Converter {
         ZoneId zone_id = ZoneId.of(zone);
         ZonedDateTime zone_date_time = now.atZone(zone_id);
         return zone_date_time;
-    }
-
-    @Override
-    public Duration timeTravel(LocalDateTime start, String startZone, LocalDateTime end, String endZone) {
-        ZoneId start_zone_id = ZoneId.of(startZone);
-        ZonedDateTime start_date_time = start.atZone(start_zone_id);
-        ZoneId end_zone_id = ZoneId.of(endZone);
-        ZonedDateTime end_date_time = end.atZone(end_zone_id);
-        Duration duration = Duration.between(start_date_time, end_date_time);
-        return duration;
     }
     
     @Override
--- Calculadora/src/main/java/calendar/Modes/IntervalMode.java
+++ Calculadora/src/main/java/calendar/Modes/IntervalMode.java
@@ -1,13 +1,12 @@
 package calendar.Modes;
 
 import calendar.Interfaces.Interval;
-import java.time.DayOfWeek;
+
+import java.time.*;
+
 import static java.time.DayOfWeek.SATURDAY;
 import static java.time.DayOfWeek.SUNDAY;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
+
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
@@ -15,10 +14,10 @@ import java.util.ArrayList;
 
 public class IntervalMode implements Interval{
 
-    @Override
-    public Duration getIntervalDateTime(Temporal d1, Temporal d2){
-        return Duration.between(d1, d2);
-    }
+//    @Override
+//    public Duration getIntervalDateTime(Temporal d1, Temporal d2){
+//        return Duration.between(d1, d2);
+//    }
     
     @Override
     public long getIntervalTimeUnit(Temporal t1, Temporal t2, ChronoUnit unit){
@@ -41,7 +40,7 @@ public class IntervalMode implements Interval{
     }
 
     @Override
-    public int numWorkingDays(Temporal t1, Temporal t2) {
+    public long numWorkingDays(Temporal t1, Temporal t2) {
         LocalDate time1 = LocalDate.from(t1);
         LocalDate time2 = LocalDate.from(t2);
         int conta = 0;
@@ -56,10 +55,29 @@ public class IntervalMode implements Interval{
     }
 
     @Override
+    public long numNonWorkingDays(Temporal t1, Temporal t2){
+        LocalDate time1 = LocalDate.from(t1);
+        LocalDate time2 = LocalDate.from(t2);
+
+        long workingdays = numWorkingDays(t1,t2);
+        return ChronoUnit.DAYS.between(t1,t2) - workingdays;
+    }
+
+    @Override
     public int getNumDayOfWeek(DayOfWeek day, Temporal start, Temporal end){
         long numDays = ChronoUnit.DAYS.between(start,end);
         numDays = numDays - Math.abs(start.get(ChronoField.DAY_OF_WEEK) - day.getValue());
         float numDayOfWeek = numDays/7;
         return (int) numDayOfWeek;
+    }
+
+    @Override
+    public Duration timeTravel(LocalDateTime start, String startZone, LocalDateTime end, String endZone) {
+        ZoneId start_zone_id = ZoneId.of(startZone);
+        ZonedDateTime start_date_time = start.atZone(start_zone_id);
+        ZoneId end_zone_id = ZoneId.of(endZone);
+        ZonedDateTime end_date_time = end.atZone(end_zone_id);
+        Duration duration = Duration.between(start_date_time, end_date_time);
+        return duration;
     }
 }  
