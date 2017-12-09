2163659e422e7ecd987a9d7ff916b5877af5ffd5
Ricardo Pereira <ricardo.agp.914@gmail.com>
2017-12-09 11:14:12 +0000

    rename Object with the main function

--- Calculadora/src/main/java/calendar/Calculator.java
+++ Calculadora/src/main/java/calendar/Calculator.java
@@ -1,9 +1,30 @@
 package calendar;
 
+import calendar.Interfaces.*;
+import calendar.Modes.CalendarMode;
+import calendar.Modes.ChronometerMode;
+import calendar.Modes.IntervalMode;
+import calendar.Modes.TimeZoneMode;
+import calendar.Presentation.Menu;
+import calendar.Util.*;
+
 /**
  *
  * @author Ricardo
  */
 public class Calculator {
-    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Interval intervalMode = new IntervalMode(); 
+        Calendar calendarMode = new CalendarMode();
+        TimeZone timeZoneMode = new TimeZoneMode();
+        Chronometer chrono = new ChronometerMode();
+        Menu home = new Menu(intervalMode, calendarMode, timeZoneMode, chrono);
+
+        home.setVisible(true);
+    }
 }
