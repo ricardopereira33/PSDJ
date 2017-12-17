b445ecc795bc35ac98b51349d199090bf7d9904b
Ricardo Pereira <ricardo.agp.914@gmail.com>
2017-12-17 13:40:03 +0000

    Test 6 done

--- Benchmark/src/main/java/Tests/Test6.java
+++ Benchmark/src/main/java/Tests/Test6.java
@@ -31,10 +31,68 @@ public class Test6 implements Test{
                                      groupingBy(t -> t.getData().getHour()))));
 
         AbstractMap.SimpleEntry<Double, Map<Month, Map<Integer, Map<Integer, List<TransCaixa>>>>> res = t.testeBoxGenW(supTable);
-        System.out.println("Time: "+ res.getKey() +"\t Res: " + res.getValue().get(0).get(1).get(1));
+        System.out.println("Time: "+ res.getKey() +"\t Res: " + res.getValue().size());
 
         /*** Java 7 ***/
+        Supplier<Map<Month, Map<Integer, Map<Integer, List<TransCaixa>>>>> supTableJava7 = () -> getTable();
+        AbstractMap.SimpleEntry<Double, Map<Month, Map<Integer, Map<Integer, List<TransCaixa>>>>> res2 = t.testeBoxGenW(supTableJava7);
+        System.out.println("Time: "+ res2.getKey() +"\t Res: " + res2.getValue().size());
+    }
 
+    private Map<Month,Map<Integer,Map<Integer,List<TransCaixa>>>> getTable() {
+        Map<Month, Map<Integer, Map<Integer, List<TransCaixa>>>> table = new HashMap<>();
 
+        for(TransCaixa t : ltc){
+            Month m = t.getData().getMonth();
+            int day =  t.getData().getDayOfMonth();
+            int hour = t.getData().getHour();
+            Map<Integer, Map<Integer, List<TransCaixa>>> tableMonth;
+            Map<Integer, List<TransCaixa>> tableDay;
+            List<TransCaixa> listHour;
+
+            tableMonth = getTableMonth(table, m);
+            tableDay = getTableDay(tableMonth, day);
+            listHour = getListHour(tableDay, hour);
+
+            listHour.add(t);
+        }
+
+        return table;
+    }
+
+    private List<TransCaixa> getListHour(Map<Integer, List<TransCaixa>> table, int hour) {
+        List<TransCaixa> listHour;
+        if(table.containsKey(hour))
+            listHour = table.get(hour);
+        else  {
+            listHour = new ArrayList<>();
+            table.put(hour, listHour);
+        }
+
+        return listHour;
+    }
+
+    private Map<Integer,List<TransCaixa>> getTableDay(Map<Integer, Map<Integer, List<TransCaixa>>> table, int day) {
+        Map<Integer, List<TransCaixa>> tableDay;
+        if(table.containsKey(day))
+            tableDay = table.get(day);
+        else  {
+            tableDay = new HashMap<>();
+            table.put(day, tableDay);
+        }
+
+        return tableDay;
+    }
+
+    private Map<Integer, Map<Integer, List<TransCaixa>>> getTableMonth(Map<Month, Map<Integer, Map<Integer, List<TransCaixa>>>> table, Month m) {
+        Map<Integer, Map<Integer, List<TransCaixa>>> tableMonth;
+        if(table.containsKey(m))
+            tableMonth = table.get(m);
+        else  {
+            tableMonth = new HashMap<>();
+            table.put(m,tableMonth);
+        }
+
+        return tableMonth;
     }
 }
