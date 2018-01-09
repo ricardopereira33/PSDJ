332d960716d5075f457b2922667d1015749d6abb
Tres <marcelo-lima96@hotmail.com>
2018-01-09 16:30:09 +0000

    testes adicionados

--- Benchmark/src/main/java/Tests/Test12.java
+++ Benchmark/src/main/java/Tests/Test12.java
@@ -4,8 +4,9 @@ import Interfaces.Test;
 import Structure.TransCaixa;
 import Utils.Tools;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
 
 public class Test12 implements Test{
     private final Tools t;
@@ -18,6 +19,42 @@ public class Test12 implements Test{
 
     @Override
     public void exe(){
+        Map<String,Map<Integer,Set<TransCaixa>>> transByCaixaMap = getTransByCaixa(false);
+        Map<String,Map<Integer,Set<TransCaixa>>> transByCaixaConcMap = getTransByCaixa(true);
 
+        Supplier<Double> supStreamMap = () -> transByCaixaMap.values().stream().mapToDouble(x -> x.values().stream().mapToDouble(h -> h.stream().mapToDouble(i -> i.getValor()).sum()).sum()).sum();
+        AbstractMap.SimpleEntry<Double, Double> res = t.testeBoxGenW(supStreamMap);
+        System.out.println("Time: "+ res.getKey() +"\t | Res: " + res.getValue());
+
+        Supplier<Double> supStreamConcMap = () -> transByCaixaConcMap.values().stream().mapToDouble(x -> x.values().stream().mapToDouble(h -> h.stream().mapToDouble(i -> i.getValor()).sum()).sum()).sum();
+        AbstractMap.SimpleEntry<Double, Double> res2 = t.testeBoxGenW(supStreamConcMap);
+        System.out.println("Time: "+ res2.getKey() +"\t | Res: " + res2.getValue());
+    }
+
+    private Map<String,Map<Integer,Set<TransCaixa>>> getTransByCaixa(boolean concurrentMap){
+        Map<String,Map<Integer,Set<TransCaixa>>> transByCaixa;
+        if(!concurrentMap) transByCaixa = new HashMap<>();
+        else transByCaixa = new ConcurrentHashMap<>();
+        for(TransCaixa tc : ltc){
+            String caixa = tc.getCaixa();
+            int month = tc.getData().getMonth().getValue();
+            Map<Integer,Set<TransCaixa>> caixaByMonth;
+            Set<TransCaixa> caixaInMonth;
+            if(transByCaixa.containsKey(caixa)) {
+                caixaByMonth = transByCaixa.get(caixa);
+            }
+            else if(!concurrentMap)caixaByMonth = new HashMap<>();
+            else caixaByMonth = new ConcurrentHashMap<>();
+            if(caixaByMonth.containsKey(month)){
+                caixaInMonth = caixaByMonth.get(month);
+            }
+            else {
+                caixaInMonth = new HashSet<>();
+            }
+            caixaInMonth.add(tc);
+            caixaByMonth.put(month,caixaInMonth);
+            transByCaixa.put(caixa,caixaByMonth);
+        }
+        return transByCaixa;
     }
 }
--- Benchmark/src/main/java/Tests/Test9.java
+++ Benchmark/src/main/java/Tests/Test9.java
@@ -4,8 +4,16 @@ import Interfaces.Test;
 import Structure.TransCaixa;
 import Utils.Tools;
 
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalField;
+import java.time.temporal.WeekFields;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.function.Supplier;
+
+import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_YEAR;
 
 public class Test9 implements Test{
     private final Tools t;
@@ -18,6 +26,47 @@ public class Test9 implements Test{
 
     @Override
     public void exe(){
+        //Para a semana 30 do ano 2017
+        List<List<TransCaixa>> tcOnWeek = getTcByWeekDays(30);
 
+        //JAVA 7
+        Supplier<Double> supForEach = () -> getTotalInWeek(tcOnWeek);
+        AbstractMap.SimpleEntry<Double, Double> res = t.testeBoxGenW(supForEach);
+        System.out.println("Time: "+ res.getKey() +"\t | Res: " + res.getValue());
+
+        //JAVA 8
+        Supplier<Double> supStream = () -> tcOnWeek.stream().mapToDouble(x -> {return x.stream().mapToDouble(TransCaixa::getValor).sum();}).sum();
+        AbstractMap.SimpleEntry<Double, Double> res2 = t.testeBoxGenW(supStream);
+        System.out.println("Time: "+ res2.getKey() +"\t | Res: " + res2.getValue());
+    }
+
+    private List<List<TransCaixa>> getTcByWeekDays(int week){
+        List<List<TransCaixa>> tcByWeekDays = new ArrayList<>();
+        for(int i = 0; i<7; i++){
+            List<TransCaixa> weekList = new ArrayList<>();
+            tcByWeekDays.add(i,weekList);
+        }
+        for(TransCaixa tc : ltc){
+            TemporalField weekOfYear = WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear();
+            LocalDateTime date = tc.getData();
+            if(date.getYear() == 2017){
+                int numWeek = date.toLocalDate().get(ALIGNED_WEEK_OF_YEAR);
+                if(numWeek == week) {
+                    int dayOfWeek = date.getDayOfWeek().getValue();
+                    tcByWeekDays.get(dayOfWeek-1).add(tc);
+                }
+            }
+        }
+        return tcByWeekDays;
+    }
+
+    private double getTotalInWeek(List<List<TransCaixa>> tcByWeekDays){
+        double total=0;
+        for(List<TransCaixa> tcWeekDay : tcByWeekDays){
+            for(TransCaixa tc : tcWeekDay){
+                total += tc.getValor();
+            }
+        }
+        return total;
     }
 }
